23eed4f0061b6fc96fc259415a2597dd8a3a8973
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-02-03 20:01:35 +0100

    remove extra charm code

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -8,29 +8,7 @@ import logging
 import typing
 
 import ops
-
 import paas_charm.go
-from paas_charm.app import App, WorkloadConfig
-
-
-class CharmApp(App):
-    """Go Charm application."""
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        """Initialize the instance.
-
-        Args:
-            args: passthrough to App.
-        """
-        super().__init__(*args, **kwargs)
-
-    def restart(self) -> None:
-        """Restart the application."""
-        self._container.add_layer(
-            "charm", self._app_layer(), combine=True)
-        self._prepare_service_for_restart()
-        self._container.replan()
-
 
 logger = logging.getLogger(__name__)
 
@@ -45,16 +23,6 @@ class SpecsCharm(paas_charm.go.Charm):
             args: passthrough to CharmBase.
         """
         super().__init__(*args)
-
-    # def _create_app(self) -> App:
-    #     """Create the application."""
-    #     charm_state = self._create_charm_state()
-    #     return CharmApp(
-    #         container=self._container,
-    #         charm_state=charm_state,
-    #         workload_config=self._workload_config,
-    #         database_migration=self._database_migration,
-    #     )
 
 
 if __name__ == "__main__":
